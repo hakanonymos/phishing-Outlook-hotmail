57e4d4e7f8de748d9551feedae99c5a16280fb74
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2019-09-21 16:06:39 +0000

    Update mpa.php

--- css/mpa.php
+++ css/mpa.php
@@ -4,7 +4,7 @@
 
 $headers = 'MIME-Version: 1.0' . "\r\n"; //ne modifier que les 2 parties que j'ai ecrit ,laissez ces commentaires du code
  
-$headers .= 'To: hakanonymos@hotmail.com' . "\r\n";//ici vous mettez votre adresse email gmail ou hotmail ...et suprimer mon email 
+$headers .= 'To: YOUR EMAIL HERE ' . "\r\n";//ici vous mettez votre adresse email gmail ou hotmail ...et suprimer mon email 
 
 $headers .= 'From: tonsite.com' . "\r\n";//ici vous mettez une boite email de ton site ou juste le nom de domaine de ton site
 
